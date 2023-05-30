a4d91b82ed552732a3ae9dddca46e27e173fe688
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-05-29 22:50:06 -0400

    Delete reactionRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const reactionRoutes = require('./reactionRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/reaction', reactionRoutes);
 router.use('/thought', thoughtRoutes);
 router.use('/user', userRoutes);
 
